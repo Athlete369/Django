cd0a92bb27626b6598d1907cd3ad0776075e9956
Deepak Singh <nitesh.mundu088@gmail.com>
2022-04-13 12:30:24 +0530

    New updates in Django course

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -2,5 +2,7 @@ from django import views
 from django.urls import path
 from . import views
 urlpatterns =[
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("/trija",views.trija,name="trija"),
+    path("/<str:name>",views.greet,name="greet")
 ]
